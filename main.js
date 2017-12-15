cdedbd36308c1929216e1817943d8c9510abdc69
James Kahng <jameskahng@Jamess-MacBook-Pro-2.local>
2017-12-14 22:54:56 -0800

    Commitopia is born. (12/14/2017)

---
+++ main.js
@@ -0,0 +1,16 @@
+//simplest application: checkout and load a single commit
+
+//is this better as a bash script that uses git directly?
+
+//api
+//authorize github api
+//request repo + commit
+//find commit diff and generate playable demo for that file (scope for now is just Pico-8)
+//repeat to generate website
+
+//potential other idea: make this website interactive. if a user requests day 60, this will generate whatever was updated on day 60.
+
+//bash path
+//git checkout
+//run some other build script
+//git checkout repeated
